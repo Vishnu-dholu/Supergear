3a960890f0470e21d5d065f8df4ca31e5094aab1
Vishnu-dholu <dholuvishnu10@gmail.com>
2024-07-06 20:36:43 +0530

    Fixed TypeScript build issue with react-paginate

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks: function (id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000, // Adjust this value as needed
+    },
+});
